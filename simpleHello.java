be87c1c3bba7270f610ecca8da7bf694254f90bf
Brendan Rodriguez <brendantr57@aol.com>
2025-05-28 18:01:06 -0400

    added all examples

--- simpleHello.java
+++ simpleHello.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class simpleHello {
     public static void main(String[] args) {
@@ -33,5 +36,35 @@ public class simpleHello {
         System.out.println("Size (HashMap): " + map.size()); // Getting size
         map.remove("Banana"); // Removing a key-value pair
         System.out.println("HashMap after removal: " + map);
+
+        // HashSet example from TA
+        HashSet<String> set = new HashSet<>();
+        set.add("Apple");
+        set.add("Banana");
+        set.add("Apple"); // Duplicate entry, will be ignored
+        System.out.println("Size (HashSet): " + set.size()); // Getting size
+        set.remove("Banana"); // Removing an element
+        System.out.println("HashSet after removal: " + set);
+
+        // TreeMap example from TA
+        TreeMap<String, Integer> treeMap = new TreeMap<>();
+        treeMap.put("Apple", 10);
+        treeMap.put("Banana", 20);
+        treeMap.put("Orange", 30);
+        System.out.println("First Key (TreeMap): " + treeMap.firstKey()); // Accessing first key
+        System.out.println("Last Key (TreeMap): " + treeMap.lastKey()); // Accessing last key
+        System.out.println("Value for key 'Banana': " + treeMap.get("Banana"));
+        treeMap.remove("Banana"); // Removing a key-value pair
+        System.out.println("TreeMap after removal: " + treeMap);
+
+        // TreeSet example from TA
+        TreeSet<String> treeSet = new TreeSet<>();
+        treeSet.add("Apple");
+        treeSet.add("Banana");
+        treeSet.add("Orange");
+        System.out.println("First Element (TreeSet): " + treeSet.first());
+        System.out.println("Last Element (TreeSet): " + treeSet.last());
+        treeSet.remove("Banana"); // Removing an element
+        System.out.println("TreeSet after removal: " + treeSet);
     }
 }
